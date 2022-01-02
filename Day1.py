96e52df11908083bd0c7e24c8f7a0a52b0d9d96d
vikaskhullar <vikas.khullar@gmail.com>
2022-01-02 20:45:22 +0530

    update

---
+++ Day1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 20:27:54 2022
+
+@author: vikas
+"""
+
---
+++ Day11.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 20:41:33 2022
+
+@author: vikas
+"""
+
